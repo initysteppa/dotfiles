df2526d6ea57fc753e3d8db52a4e272315feba46
Magnus Helmersson <mhelm@kth.se>
2013-03-01 11:53:03 +0100

    Remove what I don't need, change OSX specifics

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,3 +8,7 @@ then
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
+
+# alias for osx-style paste board usage: command | pbcopy
+alias pbcopy='xclip -selection clipboard'
+alias pbpaste='xclip -selection clipboard -o'
